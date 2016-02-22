1bc289ee383abc3109f987f14f5f7fab45fb558b
Anton Petrov <petrov.anton@live.com>
2016-02-22 21:01:05 +0300

    changes

---
+++ src/edu/petrov/lessons/collections/Main.java
@@ -0,0 +1,17 @@
+package edu.petrov.lessons.collections;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    // foo
+    private static void foo() {
+
+    }
+
+}
--- src/edu/petrov/lessons/generics/Main.java
+++ src/edu/petrov/lessons/generics/Main.java
@@ -1,9 +1,6 @@
 package edu.petrov.lessons.generics;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 
 
 public class Main {
@@ -47,6 +44,8 @@ public class Main {
 
     static class C extends B {
 
+        public C() {}
+
         public C(int value) {
             this.value = value;
         }
@@ -58,33 +57,78 @@ public class Main {
 
     }
 
+    static class D extends C {
+
+        public D() {}
+
+        public D(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public void printValue() {
+            System.out.println("D, " + value);
+        }
+
+    }
+
     public static void main(String[] args) {
-        String[] words = { "Anton", "Petrov", "Vasya", "Petya", "123456" };
-        Pair<String> mm = ArrayAlg.minmax(words);
-        Object[] objects = new Object[1];
+//        String[] words = { "Anton", "Petrov", "Vasya", "Petya", "123456" };
+//        Pair<String> mm = ArrayAlg.minmax(words);
+//        Object[] objects = new Object[1];
+//
+//        Pair<String> p = Pair.create(String.class);
+//
+//        System.out.println(mm.getFirst());
+//        System.out.println(mm.getSecond());
+//        System.out.println(ArrayAlg.getMiddle(words));
+//
+//        List<A> aList = new ArrayList<>();
+//        List<B> bList = new ArrayList<>();
+//        List<C> cList = new ArrayList<>();
+//        List<D> dList = new ArrayList<>();
+//
+//        List<Object> oList = new ArrayList<>();
+//
+//        // contravariant
+//        List<? super D> ddList = aList;
+//
+//        // covariant
+//        List<? extends A> aaList = dList;
+//        A a = aaList.get(0);
+//
+//        fill(aList); // covariance
+////        fill(bList); // error
+//        fill(oList); // contravariance
+//
+//        print(bList);
+//
+//        List<A> mList = makeList(bList);
+//
+//        fill(mList);
 
-        Pair<String> p = Pair.create(String.class);
+        foo();
+    }
 
-        System.out.println(mm.getFirst());
-        System.out.println(mm.getSecond());
-        System.out.println(ArrayAlg.getMiddle(words));
+    // foo
+    private static void foo() {
+        List<Object> objs = Arrays.asList(2, 3.14, "four");
+        List<Integer> ints = Arrays.asList(5, 6);
+        Collections.copy(objs, ints);
+        assert objs.toString().equals("[5, 6, four]");
 
-        List<A> aList = new ArrayList<>();
-        List<B> bList = new ArrayList<>();
-        List<Object> oList = new ArrayList<>();
-
-        fill(aList); // covariance
-        fill(oList); // contravariance
-
-        print(aList);
-
-        List<A> mList = makeList(bList);
-
-        fill(mList);
+//        List<Integer> ints = Arrays.asList(1,2,3);
+//        List<? extends Number> nums = ints;
+//        double dbl = sum(nums); // ok
+//        nums.add(3.14); // compile-time error
+//        List<Object> objs = Arrays.<Object>asList(1,"two");
+//        List<? super Integer> ints = objs;
+//        ints.add(3); // ok
+//        double dbl = sum(ints); // compile-time error
     }
 
     /**
-     * Covariance
+     * Covariant
      * @param list Любой класс, начиная с А и все его потомки
      */
     private static void print(List<? extends A> list) {
@@ -96,7 +140,7 @@ public class Main {
     }
 
     /**
-     * Contravariance
+     * Contravariant
      * @param list Любой класс, начиная с А и выше до Object
      */
     private static void fill(List<? super A> list) {
